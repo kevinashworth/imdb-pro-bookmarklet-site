48cda4d153d4c571ec3a350a8fc3c3566f980012
Kevin Ashworth <kevinashworth@yahoo.com>
2013-12-19 11:17:32 -0800

    new loop with pro-labs
    change to a loop from m/www to pro to pro-labs to www, as you run
    bookmarklet repeatedly

--- bookmarklet-expanded.js
+++ bookmarklet-expanded.js
@@ -1,9 +1,8 @@
 The bookmarklet:
-javascript:h=location.href;i=h.indexOf(%27imdb.com%27);p=h.indexOf(%27http://pro.imdb.com%27);l=h.indexOf(%27https://pro-labs.imdb.com%27);t=h.indexOf(%27title%27);c=h.indexOf(%27combined%27);f=h.indexOf(%27fullcredits%27);if(i==-1){window.location=%27http://pro.imdb.com/name/nm2825198/%27}else%20if(p==0){window.location=h.replace(%27http://pro%27,%27http://www%27)}else%20if(l==0){window.location=h.replace(%27https://pro-labs%27,%27http://www%27)}else%20if((p==-1)&&(l==-1)){if((t>1)&&(c>1)){h=h.substring(0,c)}else%20if((t>1)&&(f>1)){h=h.substring(0,f+11)};window.location=h.replace(/http:\/\/[a-z]+/,%27http://pro%27);};
+javascript:h=location.href;i=h.indexOf(%27imdb.com%27);p=h.indexOf(%27http://pro.imdb.com%27);l=h.indexOf(%27https://pro-labs.imdb.com%27);t=h.indexOf(%27title%27);c=h.indexOf(%27combined%27);f=h.indexOf(%27fullcredits%27);if(i==-1){window.location=%27http://pro.imdb.com/name/nm2825198/%27}else%20if(p==0){window.location=h.replace(%27http://pro%27,%27https://pro-labs%27)}else%20if(l==0){window.location=h.replace(%27https://pro-labs%27,%27http://www%27)}else%20if((p==-1)&&(l==-1)){if((t>1)&&(c>1)){h=h.substring(0,c)}else%20if((t>1)&&(f>1)){h=h.substring(0,f+11)};window.location=h.replace(/http:\/\/[a-z]+/,%27http://pro%27);};
 
 The bookmarklet with line breaks and unencoded:
 
-// 
 h=location.href;
 i=h.indexOf('imdb.com');
 p=h.indexOf('http://pro.imdb.com');
@@ -15,7 +14,7 @@ if(i==-1){
   window.location='http://pro.imdb.com/name/nm2825198/'
 }
 else if(p==0){
-  window.location=h.replace('http://pro','http://www')
+  window.location=h.replace('http://pro','https://pro-labs')
 }
 else if(l==0){
   window.location=h.replace('https://pro-labs','http://www')
