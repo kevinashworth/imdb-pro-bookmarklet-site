dcd06987ae48d76dd0605b135935190e5a867705
kevinashworth <kevinashworth@yahoo.com>
2014-04-13 13:53:25 -0700

    Update bookmarklet-expanded.js
    Remove 'pro' -- just 'pro-labs' now.

--- bookmarklet-expanded.js
+++ bookmarklet-expanded.js
@@ -1,5 +1,5 @@
 The bookmarklet:
-javascript:h=location.href;i=h.indexOf(%27imdb.com%27);p=h.indexOf(%27https://pro-labs.imdb.com%27);t=h.indexOf(%27title%27);c=h.indexOf(%27combined%27);f=h.indexOf(%27fullcredits%27);if(i==-1){window.location=%27http://pro.imdb.com/name/nm2825198/%27}else%20if(p==0){window.location=h.replace(%27https://pro-labs%27,%27http://www%27)}else%20if(p==-1){if((t>1)&&(c>1)){h=h.substring(0,c)}else%20if((t>1)&&(f>1)){h=h.substring(0,f+11)};window.location=h.replace(/http:\/\/[a-z]+/,%27http://pro-labs%27);};
+javascript:h=location.href;i=h.indexOf(%27imdb.com%27);p=h.indexOf(%27https://pro-labs.imdb.com%27);t=h.indexOf(%27title%27);c=h.indexOf(%27combined%27);f=h.indexOf(%27fullcredits%27);if(i==-1){window.location=%27http://pro-labs.imdb.com/name/nm2825198/%27}else%20if(p==0){window.location=h.replace(%27https://pro-labs%27,%27http://www%27)}else%20if(p==-1){if((t>1)&&(c>1)){h=h.substring(0,c)}else%20if((t>1)&&(f>1)){h=h.substring(0,f+11)};window.location=h.replace(/http:\/\/[a-z]+/,%27http://pro-labs%27);};
 
 The bookmarklet with line breaks and unencoded:
 
@@ -10,7 +10,7 @@ t=h.indexOf('title');
 c=h.indexOf('combined');
 f=h.indexOf('fullcredits');
 if(i==-1){
-  window.location='http://pro.imdb.com/name/nm2825198/'
+  window.location='http://pro-labs.imdb.com/name/nm2825198/'
 }
 else if(p==0){
   window.location=h.replace('https://pro-labs','http://www')
